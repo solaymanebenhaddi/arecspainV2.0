5202233ccea17b5ad42c1b28de224d56a07b905f
solaymanebenhaddi <heddoudi.mohamed@gmail.com>
2022-06-25 19:37:44 +0000

    update header

--- src/components/propertyItem/PropertyItem.jsx
+++ src/components/propertyItem/PropertyItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./propertyItem.css";
 
 function PropertyItem({ item }) {
   return (
-    <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+    <div className="col-lg-3 col-md-3 wow fadeInUp" data-wow-delay="0.1s">
       <div className="property-item rounded overflow-hidden">
         <div className="position-relative overflow-hidden">
           <a href="/#">
@@ -12,7 +13,7 @@ function PropertyItem({ item }) {
             For Sell
           </div>
           <div className="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
-            Appartment
+           {item.type}
           </div>
         </div>
         <div className="p-4 pb-0">
